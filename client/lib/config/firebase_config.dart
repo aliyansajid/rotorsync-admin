bf2af72c77097d2e8924e37b3c3e7bfe0f8ddddd
Aliyan Sajid <aliyansajid127@gmail.com>
2025-03-01 22:04:42 +0500

    Firebase setup for android, ios and web

---
+++ client/lib/config/firebase_config.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
+
+class FirebaseConfig {
+  static FirebaseOptions get options {
+    return const FirebaseOptions(
+        apiKey: "AIzaSyCkDWOcrCccqPE7cK0yzK0Ax6zyJlO2C6A",
+        authDomain: "rotorsync-admin.firebaseapp.com",
+        projectId: "rotorsync-admin",
+        storageBucket: "rotorsync-admin.firebasestorage.app",
+        messagingSenderId: "516482601733",
+        appId: "1:516482601733:web:9bb3a4447c965e01ea9d6c",
+        measurementId: "G-KTZTLG14NL");
+  }
+}
